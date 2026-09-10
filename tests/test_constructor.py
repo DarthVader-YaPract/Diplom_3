46734ff4e4196a57ff22f1864635beede1bd1a78
Andrey Kalashov <incomin-post@yandex.ru>
2026-09-10 23:35:01 +0000

    UI-тесты сгруппированы по функциональности

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -2,23 +2,20 @@ import allure
 
 
 @allure.feature("Конструктор")
-@allure.title("Открытие деталей ингредиента")
-def test_ingredient_card_opens_details(constructor_page):
-    constructor_page.open_ingredient()
-    assert constructor_page.ingredient_details_are_open()
+class TestConstructor:
+    @allure.title("Открытие деталей ингредиента")
+    def test_ingredient_card_opens_details(self, constructor_page):
+        constructor_page.open_ingredient()
+        assert constructor_page.ingredient_details_are_open()
 
+    @allure.title("Закрытие деталей по крестику")
+    def test_cross_closes_ingredient_details(self, constructor_page):
+        constructor_page.open_ingredient()
+        constructor_page.close_ingredient()
+        assert constructor_page.ingredient_details_are_closed()
 
-@allure.feature("Конструктор")
-@allure.title("Закрытие деталей по крестику")
-def test_cross_closes_ingredient_details(constructor_page):
-    constructor_page.open_ingredient()
-    constructor_page.close_ingredient()
-    assert constructor_page.ingredient_details_are_closed()
-
-
-@allure.feature("Конструктор")
-@allure.title("Увеличение счётчика ингредиента")
-def test_added_sauce_increases_counter(constructor_page):
-    before = constructor_page.sauce_count()
-    after = constructor_page.add_sauce()
-    assert after > before
+    @allure.title("Увеличение счётчика ингредиента")
+    def test_added_sauce_increases_counter(self, constructor_page):
+        before = constructor_page.sauce_count()
+        after = constructor_page.add_sauce()
+        assert after > before
--- tests/test_order_feed.py
+++ tests/test_order_feed.py
@@ -2,30 +2,33 @@ import allure
 
 
 @allure.feature("Лента заказов")
-@allure.title("Изменение общего счётчика")
-def test_completed_order_changes_total_count(signed_in_constructor, feed):
-    feed.open()
-    before = feed.total_count()
-    feed.go_to_constructor().create_order()
-    feed.open()
-    after = feed.wait_total_increase(before)
-    assert after > before
+class TestOrderFeed:
+    @allure.title("Изменение общего счётчика")
+    def test_completed_order_changes_total_count(
+        self, signed_in_constructor, feed
+    ):
+        feed.open()
+        before = feed.total_count()
+        feed.go_to_constructor().create_order()
+        feed.open()
+        after = feed.wait_total_increase(before)
+        assert after > before
 
+    @allure.title("Изменение сегодняшнего счётчика")
+    def test_completed_order_changes_today_count(
+        self, signed_in_constructor, feed
+    ):
+        feed.open()
+        before = feed.today_count()
+        feed.go_to_constructor().create_order()
+        feed.open()
+        after = feed.wait_today_increase(before)
+        assert after > before
 
-@allure.feature("Лента заказов")
-@allure.title("Изменение сегодняшнего счётчика")
-def test_completed_order_changes_today_count(signed_in_constructor, feed):
-    feed.open()
-    before = feed.today_count()
-    feed.go_to_constructor().create_order()
-    feed.open()
-    after = feed.wait_today_increase(before)
-    assert after > before
-
-
-@allure.feature("Лента заказов")
-@allure.title("Заказ появляется в работе")
-def test_completed_order_appears_in_progress(signed_in_constructor, feed):
-    order_number = signed_in_constructor.create_order()
-    feed.open()
-    assert feed.order_appears_in_progress(order_number)
+    @allure.title("Заказ появляется в работе")
+    def test_completed_order_appears_in_progress(
+        self, signed_in_constructor, feed
+    ):
+        order_number = signed_in_constructor.create_order()
+        feed.open()
+        assert feed.order_appears_in_progress(order_number)
